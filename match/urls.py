ea226dd4d5a25ffc10005bb5716530cecb9d39b4
sheph2mj <sheph2mj@cmich.edu>
2015-07-24 16:39:20 -0400

    game form base structure with no styling
    also add dsstore to .gitignore

--- match/urls.py
+++ match/urls.py
@@ -3,8 +3,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from .views import Index
+from .views import Index, CreateGame
 
 urlpatterns = [
     url(r'^$', Index.as_view(), name="index"),
+    url(r'^create-game/$', CreateGame.as_view(), name="create_game"),
 ]
--- match/views.py
+++ match/views.py
@@ -1,7 +1,17 @@
 from django.views.generic import TemplateView
 
+from nine_oh_lb.settings import CHAMPION_STRINGS
+
 
 class Index(TemplateView):
 
 	template_name = "match/base_site.html"
 
+class CreateGame(TemplateView):
+
+	template_name = "match/game_form.html"
+
+	def get_context_data(self, *args, **kwargs):
+		context = {}
+		context['champ_names'] = CHAMPION_STRINGS
+		return context
